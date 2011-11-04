6165b6b6f4abf3c1e27bd1dd8fcb26af9cecca68
Sean Fisk <sean@seanfisk.com>
2011-11-04 11:17:25 -0400

    Add build_fish script.

---
+++ build_fish.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# fish does not support out-of-source builds
+source install_scripts.bash
+
+download_and_extract http://fishshell.com/files/1.23.1/fish-1.23.1.tar.bz2
+cd $src_dir_name
+./configure --prefix="$HOME/.local" CPPFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncursesw" LDFLAGS="-L$HOME/.local/lib -L$HOME/.local/lib64"
+make_install
