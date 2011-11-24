c4958c1571c55402953a7a267ac1a6466e17ebde
Sean Fisk <sean@seanfisk.com>
2011-11-23 19:20:36 -0500

    Correct readline and ncurses.

--- ncurses.bash
+++ ncurses.bash
@@ -2,6 +2,6 @@
 
 source build-systems/autotools.bash
 
-EXTRA_CONFIGURE_FLAGS=(--enable-termcap --enable-widec)
+EXTRA_CONFIGURE_FLAGS=(--with-shared)
 autotools http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
 
--- readline.bash
+++ readline.bash
@@ -2,5 +2,4 @@
 
 source build-systems/autotools.bash
 
-readonly EXTRA_CONFIGURE_FLAGS=(--with-curses)
 autotools http://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz
