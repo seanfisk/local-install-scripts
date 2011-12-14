78b1ddabe02e03c7af81dd8324bdb0870890e31c
Sean Fisk <sean@seanfisk.com>
2011-12-14 02:05:13 -0500

    Fix the git installer

--- git.bash
+++ git.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+source build-systems/autotools.bash
+
 download_and_extract http://git-core.googlecode.com/files/git-1.7.8.tar.gz
 
+cd $src_dir_name
 readonly MAKE_FLAGS=("prefix=$PREFIX")
 EXTRA_MAKE_FLAGS=${MAKE_FLAGS[@]}
 EXTRA_MAKE_INSTALL_FLAGS=${MAKE_FLAGS[@]}
