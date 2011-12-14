5dd4c4c416231a7f96d8ad42c61796e59de8d26a
Sean Fisk <sean@seanfisk.com>
2011-12-14 02:25:53 -0500

    Add installer for xclip

---
+++ xclip.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# xclip does not support out-of-source builds
+
+source install_scripts.bash
+
+download_and_extract http://sourceforge.net/projects/xclip/files/xclip/0.12/xclip-0.12.tar.gz
+
+cd $src_dir_name
+./configure --prefix="$PREFIX"
+make_install
+
