76b2e99e1072cc516dfeb1146a3c0e906423eef7
Sean Fisk <sean@seanfisk.com>
2012-01-25 12:14:49 -0500

    Fix openssl

--- openssl.bash
+++ openssl.bash
@@ -3,9 +3,14 @@
 # openssl does not support out-of-source builds in a nice way
 source install_scripts.bash
 
-download_and_extract http://www.openssl.org/source/openssl-1.0.0e.tar.gz
+download_and_extract http://www.openssl.org/source/openssl-0.9.8s.tar.gz
+# too new for ruby
+# download_and_extract http://www.openssl.org/source/openssl-1.0.0f.tar.gz
 cd $src_dir_name
-./config --prefix="$PREFIX"
-readonly EXTRA_MAKE_STEPS=(test)
-make_install
+./config --prefix="$PREFIX" shared
+# the tests fail for some reason
+# readonly EXTRA_MAKE_STEPS=(test)
+make --jobs=1
+# has problems with multiple jobs
+make --jobs=1 install
 
