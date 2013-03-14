2f7f535e9a00dc51cb1f81cffb1fc923ceebb4b2
Sean Fisk <sean@seanfisk.com>
2013-03-13 23:57:54 -0400

    Upgrade to Emacs 24.3.

--- emacs.bash
+++ emacs.bash
@@ -5,4 +5,4 @@
 source build-systems/autotools.bash
 
 EXTRA_CONFIGURE_FLAGS=(--with-gif=no --with-xpm=no)
-autotools ftp://ftp.gnu.org/gnu/emacs/emacs-24.2.tar.xz
+autotools ftp://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.xz
