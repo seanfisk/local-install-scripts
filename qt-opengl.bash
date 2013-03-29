40f84a421d2546a0638009f6ac041f738520f549
Sean Fisk <sean@seanfisk.com>
2013-03-28 20:07:24 -0400

    Add Qt installer for using with OpenGL.

---
+++ qt-opengl.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source build-systems/autotools.bash
+
+EXTRA_CONFIGURE_FLAGS=(
+	-opensource
+	-shared
+	-fast
+	-no-qt3support
+	-no-xmlpatterns
+	-no-multimedia
+	-no-audio-backend
+	-no-phonon
+	-no-phonon-backend
+	-no-svg
+	-no-webkit
+	-no-javascript-jit
+	-no-script
+	-no-scripttools
+	-no-declarative
+	-no-declarative-debug
+	-no-nis
+	-no-cups
+	-no-dbus
+	-opengl
+	-no-openvg
+)
+autotools http://releases.qt-project.org/qt4/source/qt-everywhere-opensource-src-4.8.4.tar.gz
