c368dd419cd8231aac8805bd55e9b75da8371b68
Sean Fisk <sean@seanfisk.com>
2013-05-22 08:48:15 -0600

    Upgrade tmux and libevent versions; take out libevent test.

--- libevent.bash
+++ libevent.bash
@@ -2,5 +2,5 @@
 
 source build-systems/autotools.bash
 
-EXTRA_MAKE_STEPS=(verify)
-autotools https://github.com/downloads/libevent/libevent/libevent-2.0.16-stable.tar.gz
+#EXTRA_MAKE_STEPS=(verify)
+autotools https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
