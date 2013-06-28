f592162422ed2277a5bc0b5671e2105adad1002f
Sean Fisk <sean@seanfisk.com>
2013-06-28 13:33:52 -0600

    Add XPM support back into compiled Emacs.

--- emacs.bash
+++ emacs.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source lib/autotools.bash
-EXTRA_CONFIGURE_FLAGS=(--with-gif=no --with-xpm=no)
+EXTRA_CONFIGURE_FLAGS=(--with-gif=no)
 autotools ftp://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.xz
