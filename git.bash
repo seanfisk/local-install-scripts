8937e6b27fdef9a66b41a32acbaef6dc91b12cc1
Sean Fisk <sean@seanfisk.com>
2013-07-11 14:16:48 -0600

    Update git version.

--- git.bash
+++ git.bash
@@ -2,7 +2,7 @@
 
 source lib/common.bash
 
-download_and_extract http://git-core.googlecode.com/files/git-1.7.8.tar.gz
+download_and_extract https://git-core.googlecode.com/files/git-1.8.3.2.tar.gz
 
 cd $src_dir_name
 readonly MAKE_FLAGS=("prefix=$PREFIX")
