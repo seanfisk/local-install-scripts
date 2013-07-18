fe91908a030e4058265f5e94031d075015ce748b
Sean Fisk <sean@seanfisk.com>
2013-07-18 10:01:08 -0600

    Add missing git man pages.

--- git.bash
+++ git.bash
@@ -2,11 +2,23 @@
 
 source lib/common.bash
 
-download_and_extract https://git-core.googlecode.com/files/git-1.8.3.2.tar.gz
+readonly GIT_VERSION=1.8.3.2
 
-cd $src_dir_name
+# Download and install git itself.
+
+download_and_extract "https://git-core.googlecode.com/files/git-$GIT_VERSION.tar.gz"
+
+pushd $src_dir_name
 readonly MAKE_FLAGS=("prefix=$PREFIX")
 EXTRA_MAKE_FLAGS=${MAKE_FLAGS[@]}
 EXTRA_MAKE_INSTALL_FLAGS=${MAKE_FLAGS[@]}
 
 make_install
+popd # $src_dir_name
+
+# Download git man pages. Strange these don't come with git.
+
+pushd $PREFIX/share/man
+wget "https://git-core.googlecode.com/files/git-manpages-$GIT_VERSION.tar.gz" --output-document - | \
+	tar -xz
+popd # $PREFIX/share/man
