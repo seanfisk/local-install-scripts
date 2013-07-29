d9ea3cf40046012e69265852f0c02da92461a5fb
Sean Fisk <sean@seanfisk.com>
2013-07-29 16:04:40 -0400

    Add XPM support to Emacs build.

--- emacs.bash
+++ emacs.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 source lib/autotools.bash
-EXTRA_CONFIGURE_FLAGS=(--with-gif=no)
+
+# On EOS, Emacs needs libXpm (specifically, for nyan-mode). The libraries are there but the headers aren't. So make sure you run `libXpm.bash' before building Emacs. We also need to manually the include and library paths so that Emacs finds libXpm. Usually this would happen automatically, but it doesn't work for this build.
+#
+# Also build without gif support.
+EXTRA_CONFIGURE_FLAGS=("CPPFLAGS=-I$PREFIX/include" "LDFLAGS=-L$PREFIX/lib" --with-gif=no)
+
 autotools ftp://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.xz
---
+++ libXpm.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source lib/autotools.bash
+autotools http://xorg.freedesktop.org/releases/individual/lib/libXpm-3.5.10.tar.bz2
