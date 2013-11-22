1948eef1afab1f896eefddc6a14536d7058f5885
Sean Fisk <sean@seanfisk.com>
2013-11-22 02:18:33 -0500

    Add rpath linker option for tmux.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 # TODO
 
+* Consider adding standard prefix includes and and rpath flag to all builds, just like the tmux script.
 * Add wemux. Will probably have to patch it to get it to work as a standard user.
 * Decide standard of hyphens (probably) or underscores (probably not) and stick to them for functions, aliases, commands, and file names.
--- sw/tmux.bash
+++ sw/tmux.bash
@@ -6,6 +6,6 @@
 source lib/autotools.bash
 
 # Alter the compiler flags so it picks up on libevent.
-EXTRA_CONFIGURE_FLAGS=("CPPFLAGS=-I$PREFIX/include" "LDFLAGS=-L$PREFIX/lib -L$PREFIX/lib64")
+EXTRA_CONFIGURE_FLAGS=("CPPFLAGS=-I$PREFIX/include" "LDFLAGS=-L$PREFIX/lib -L$PREFIX/lib64 -Wl,-rpath,$PREFIX/lib")
 readonly TMUX_VERSION=1.8
 autotools "http://sourceforge.net/projects/tmux/files/tmux/tmux-$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz"
