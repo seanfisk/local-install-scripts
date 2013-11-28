c892858a1c833a5d7428f694fef477098ae801ab
Sean Fisk <sean@seanfisk.com>
2013-11-27 21:52:12 -0500

    Adapt Qt to new autotools.

--- sw/qt_opengl.bash
+++ sw/qt_opengl.bash
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
 
-source lib/autotools.bash
-EXTRA_CONFIGURE_FLAGS=(
-	-opensource
-	-shared
-	-fast
-	-no-qt3support
-	-no-xmlpatterns
-	-no-multimedia
-	-no-audio-backend
-	-no-phonon
-	-no-phonon-backend
-	-no-svg
-	-no-webkit
-	-no-javascript-jit
-	-no-script
-	-no-scripttools
-	-no-declarative
-	-no-declarative-debug
-	-no-nis
-	-no-cups
-	-no-dbus
-	-opengl
+source lib/build.bash
+
+readonly QT_VERSION=4.8.5
+
+goto_src_dir
+
+download_and_extract "http://download.qt-project.org/official_releases/qt/4.8/$QT_VERSION/qt-everywhere-opensource-src-$QT_VERSION.tar.gz"
+configure_path="$PWD/$src_dir_name/configure"
+
+create_cd_build_dir $src_dir_name
+
+"$configure_path" \
+	-prefix "$PREFIX" \
+	-opensource \
+	-confirm-license \
+	-shared \
+	-fast \
+	-no-qt3support \
+	-no-audio-backend \
+	-no-phonon \
+	-no-phonon-backend \
+	-no-svg \
+	-no-webkit \
+	-no-javascript-jit \
+	-no-script \
+	-no-scripttools \
+	-no-declarative \
+	-no-declarative-debug \
+	-no-nis \
+	-no-cups \
+	-no-dbus \
+	-opengl \
 	-no-openvg
-)
-autotools http://releases.qt-project.org/qt4/source/qt-everywhere-opensource-src-4.8.4.tar.gz
+
+make
+make install
