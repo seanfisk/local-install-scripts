9869f7c026e3e45fe99462777f4b729915d90192
Sean Fisk <sean@seanfisk.com>
2013-12-11 17:41:51 -0500

    Add PKG_CONFIG_PATH that points to installed pc files.

--- lib/config.bash
+++ lib/config.bash
@@ -8,3 +8,6 @@ set -o xtrace
 readonly PREFIX=$HOME/.local
 readonly SOURCE_DIR=$PREFIX/src
 readonly BUILD_DIR=$PREFIX/build
+
+# Set this for pkg-config so that our local packages are found.
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:${PKG_CONFIG_PATH:+PKG_CONFIG_PATH}
